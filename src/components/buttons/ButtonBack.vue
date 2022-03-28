86f3695920d2de03b701acf2dd579b6a9a24739f
luiz.eduardo <luiz.eduardo@usemobile.xyz>
2022-03-28 11:55:10 -0300

    feat: bind class attribute to render dynamic style

--- src/components/buttons/ButtonBack.vue
+++ src/components/buttons/ButtonBack.vue
@@ -1,5 +1,9 @@
 <template>
-  <button @click="router.back()" :class="`rounded-lg bg-${color} px-3 py-2`">
+  <button
+    @click="router.back()"
+    class="rounded-lg px-3 py-2"
+    :class="`bg-${color}`"
+  >
     <span class="text-white font-semibold">{{ text }}</span>
   </button>
 </template>
