8332647c7cdb8354f495add8b6ebdcd27c7f306e
luiz.eduardo <luiz.eduardo@usemobile.xyz>
2022-04-07 14:06:51 -0300

    feat: added movies to mocks

--- src/store/helper.ts
+++ src/store/helper.ts
@@ -76,6 +76,82 @@ const mocks: any = {
         birthday: '27/03/1963',
       },
     },
+    {
+      name: 'Bastardos Inglórios',
+      sinopse:
+        'Durante a Segunda Guerra Mundial, na França, um grupo de judeus americanos conhecidos como Bastardos espalha o terror entre o terceiro Reich.',
+      year: 2009,
+      imdb: 8.3,
+      imdbLink: 'https://www.imdb.com/title/tt0361748/',
+      actors: [
+        {
+          name: 'Christoph Waltz',
+          birthday: '04/09/1956',
+          photo:
+            'http://t2.gstatic.com/licensed-image?q=tbn:ANd9GcQMwzyxm368J2fXlDYvy3IcunsLwQrlKlPy76QvFIRNaObZgnWlYoYsSAFUaaWd',
+          age: 65,
+        },
+        {
+          name: 'Brad Pitt',
+          birthday: '18/12/1963',
+          photo:
+            'https://conteudo.imguol.com.br/c/entretenimento/33/2022/01/05/brad-pitt-pode-estrelar-filme-sobre-formula-1-1641393478695_v2_3x4.jpg',
+          age: 58,
+        },
+        {
+          name: 'Eli Roth',
+          birthday: '18/04/1972',
+          photo:
+            'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRbTxGsFqhsnsCorXk6G3JodZngfmzO3uNdHJQ5ilYNpA24Yx9r',
+          age: 49,
+        },
+      ],
+      poster:
+        'https://uauposters.com.br/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/1/5/157720140525-uau-posters-filmes-bastardos-inglorios--inglourious-basterds-5-2.jpg',
+      director: {
+        name: 'Quentin Tarantino',
+        age: 59,
+        birthday: '27/03/1963',
+      },
+    },
+    {
+      name: 'Os Oito Odiados',
+      sinopse:
+        'Em busca de abrigo para se proteger de uma nevasca, dois caçadores de recompensas, um prisioneiro e um homem que alega ser xerife conhecem quatro estranhos.',
+      year: 2016,
+      imdb: 7.8,
+      imdbLink: 'https://www.imdb.com/title/tt3460252/',
+      actors: [
+        {
+          name: 'Samuel L. Jackson',
+          birthday: '21/12/1948',
+          photo:
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Samuel_L._Jackson_2019_by_Glenn_Francis.jpg/1200px-Samuel_L._Jackson_2019_by_Glenn_Francis.jpg',
+          age: 73,
+        },
+        {
+          name: 'Kurt Russell',
+          birthday: '17/03/1951',
+          photo:
+            'http://t2.gstatic.com/licensed-image?q=tbn:ANd9GcR7iF152wZ9u1mZOEwsnOzXPbcP-AEmiHuGG9Q3vyyun_Hw5x9J-DoCiaJlgTsc',
+          age: 71,
+        },
+        {
+          name: 'Jennifer Jason Leigh',
+          birthday: '05/02/1962',
+          photo:
+            'http://t0.gstatic.com/licensed-image?q=tbn:ANd9GcRRqKOma4zQ56RYKooCyhVa7GbGwVBLWdmq7sfqOcevNM3iH7UUUyskFXq6tvui',
+          age: 60,
+        },
+      ],
+      poster:
+        'https://img.elo7.com.br/product/zoom/2E955E1/big-poster-filme-os-oito-odiados-lo002-tamanho-90x60-cm-quadro.jpg',
+      director: {
+        name: 'Quentin Tarantino',
+        age: 59,
+        birthday: '27/03/1963',
+      },
+    },
   ],
   directors: [
     {
