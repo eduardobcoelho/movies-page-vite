58c2f16bb021cebc6b1e89f7da771bf7bb70cdd3
luiz.eduardo <luiz.eduardo@usemobile.xyz>
2022-04-08 21:39:06 -0300

    refactor: adjust mocks data

--- src/store/helper.ts
+++ src/store/helper.ts
@@ -1,6 +1,43 @@
 const mocks: any = {
   movies: [
     {
+      name: 'Cães de Aluguel',
+      sinopse: 'Criminoso reúne seis bandidos para grande roubo de diamantes.',
+      year: 1992,
+      imdb: 8.3,
+      imdbLink: 'https://www.imdb.com/title/tt0105236/',
+      actors: [
+        {
+          name: 'Quentin Tarantino',
+          age: 59,
+          photo:
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxlTrJvdxqSMBYf90USQe0qXEaMhXdy35FJOpUlEZ5PGl4wIBI',
+          birthday: '27/03/1963',
+        },
+        {
+          name: 'Tim Roth',
+          birthday: '14/05/1961',
+          photo:
+            'http://t1.gstatic.com/licensed-image?q=tbn:ANd9GcSh4JlBCQR0GBv8QAgpQ8GjVjEYcU2QMDFGaR2uOkLuhi792XYuRTzN0wmo_PiY',
+          age: 60,
+        },
+        {
+          name: 'Steve Buscemi',
+          birthday: '13/12/1957',
+          photo:
+            'http://t2.gstatic.com/licensed-image?q=tbn:ANd9GcQmKopVNGoxsTJ1DRo4jO1MvmuzprjylNr309oeqLtF44eVwK5Se5C0gCRpeolR',
+          age: 64,
+        },
+      ],
+      poster:
+        'https://br.web.img3.acsta.net/pictures/15/03/19/17/47/301476.jpg',
+      director: {
+        name: 'Quentin Tarantino',
+        age: 59,
+        birthday: '27/03/1963',
+      },
+    },
+    {
       name: 'Pulp fiction',
       sinopse:
         'Assassino que trabalha para a máfia se apaixona pela esposa de seu chefe quando é convidado a acompanhá-la.',
@@ -32,44 +69,6 @@ const mocks: any = {
       ],
       poster:
         'https://img.elo7.com.br/product/original/268A58D/big-poster-filme-pulp-fiction-lo001-tamanho-90x60-cm-presente-geek.jpg',
-      director: {
-        name: 'Quentin Tarantino',
-        age: 59,
-        birthday: '27/03/1963',
-      },
-    },
-    {
-      name: 'Django Livre',
-      sinopse:
-        'No sul dos Estados Unidos, o ex-escravo Django faz uma aliança inesperada com o caçador de recompensas Schultz para caçar os criminosos mais procurados do país e resgatar sua esposa.',
-      year: 2012,
-      imdb: 8.5,
-      imdbLink: 'https://www.imdb.com/title/tt1853728/',
-      actors: [
-        {
-          name: 'Jamie Foxx',
-          birthday: '13/12/1967',
-          photo:
-            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTf6FGrcpWKJc-y8maa2E5u33sWL4KQXBV2FtNmaV9jfih5GJ0e',
-          age: 54,
-        },
-        {
-          name: 'Leonardo DiCaprio',
-          birthday: '11/11/1974',
-          photo:
-            'https://br.web.img2.acsta.net/c_310_420/medias/nmedia/18/92/61/22/20208787.jpg',
-          age: 47,
-        },
-        {
-          name: 'Samuel L. Jackson',
-          birthday: '21/12/1948',
-          photo:
-            'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Samuel_L._Jackson_2019_by_Glenn_Francis.jpg/1200px-Samuel_L._Jackson_2019_by_Glenn_Francis.jpg',
-          age: 73,
-        },
-      ],
-      poster:
-        'https://br.web.img2.acsta.net/medias/nmedia/18/90/07/53/20391069.jpg',
       director: {
         name: 'Quentin Tarantino',
         age: 59,
@@ -115,10 +114,48 @@ const mocks: any = {
       },
     },
     {
+      name: 'Django Livre',
+      sinopse:
+        'No sul dos Estados Unidos, o ex-escravo Django faz uma aliança inesperada com o caçador de recompensas Schultz para caçar os criminosos mais procurados do país e resgatar sua esposa.',
+      year: 2012,
+      imdb: 8.5,
+      imdbLink: 'https://www.imdb.com/title/tt1853728/',
+      actors: [
+        {
+          name: 'Jamie Foxx',
+          birthday: '13/12/1967',
+          photo:
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTf6FGrcpWKJc-y8maa2E5u33sWL4KQXBV2FtNmaV9jfih5GJ0e',
+          age: 54,
+        },
+        {
+          name: 'Leonardo DiCaprio',
+          birthday: '11/11/1974',
+          photo:
+            'https://br.web.img2.acsta.net/c_310_420/medias/nmedia/18/92/61/22/20208787.jpg',
+          age: 47,
+        },
+        {
+          name: 'Samuel L. Jackson',
+          birthday: '21/12/1948',
+          photo:
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Samuel_L._Jackson_2019_by_Glenn_Francis.jpg/1200px-Samuel_L._Jackson_2019_by_Glenn_Francis.jpg',
+          age: 73,
+        },
+      ],
+      poster:
+        'https://br.web.img2.acsta.net/medias/nmedia/18/90/07/53/20391069.jpg',
+      director: {
+        name: 'Quentin Tarantino',
+        age: 59,
+        birthday: '27/03/1963',
+      },
+    },
+    {
       name: 'Os Oito Odiados',
       sinopse:
         'Em busca de abrigo para se proteger de uma nevasca, dois caçadores de recompensas, um prisioneiro e um homem que alega ser xerife conhecem quatro estranhos.',
-      year: 2016,
+      year: 2015,
       imdb: 7.8,
       imdbLink: 'https://www.imdb.com/title/tt3460252/',
       actors: [
@@ -146,43 +183,6 @@ const mocks: any = {
       ],
       poster:
         'https://img.elo7.com.br/product/zoom/2E955E1/big-poster-filme-os-oito-odiados-lo002-tamanho-90x60-cm-quadro.jpg',
-      director: {
-        name: 'Quentin Tarantino',
-        age: 59,
-        birthday: '27/03/1963',
-      },
-    },
-    {
-      name: 'Cães de Aluguel',
-      sinopse: 'Criminoso reúne seis bandidos para grande roubo de diamantes.',
-      year: 1993,
-      imdb: 8.3,
-      imdbLink: 'https://www.imdb.com/title/tt0105236/',
-      actors: [
-        {
-          name: 'Quentin Tarantino',
-          age: 59,
-          photo:
-            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxlTrJvdxqSMBYf90USQe0qXEaMhXdy35FJOpUlEZ5PGl4wIBI',
-          birthday: '27/03/1963',
-        },
-        {
-          name: 'Tim Roth',
-          birthday: '14/05/1961',
-          photo:
-            'http://t1.gstatic.com/licensed-image?q=tbn:ANd9GcSh4JlBCQR0GBv8QAgpQ8GjVjEYcU2QMDFGaR2uOkLuhi792XYuRTzN0wmo_PiY',
-          age: 60,
-        },
-        {
-          name: 'Steve Buscemi',
-          birthday: '13/12/1957',
-          photo:
-            'http://t2.gstatic.com/licensed-image?q=tbn:ANd9GcQmKopVNGoxsTJ1DRo4jO1MvmuzprjylNr309oeqLtF44eVwK5Se5C0gCRpeolR',
-          age: 64,
-        },
-      ],
-      poster:
-        'https://br.web.img3.acsta.net/pictures/15/03/19/17/47/301476.jpg',
       director: {
         name: 'Quentin Tarantino',
         age: 59,
